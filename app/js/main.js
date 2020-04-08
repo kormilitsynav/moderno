9e2eac4823b77543dc881d840e074b2b88c316d4
kormilitsynav <kormilicyn.av@gmail.com>
2020-04-08 07:07:35 +0300

    Added product-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,16 @@
 $(function(){
   var mixer = mixitup('.products__inner-box');
 
-
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
   
+  $('.product-slider__inner').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
 });
