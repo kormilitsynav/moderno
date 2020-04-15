3b3aea7c641b3be3b22a407ecb4692281775760a
kormilitsynav <kormilicyn.av@gmail.com>
2020-04-15 06:09:06 +0300

    Added adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -35,7 +35,16 @@ $(function(){
     $('.icon-th-list').removeClass('active');
   });
 
+  
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
 
+  $('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+  });
+
+  
   $(window).on("load", function () {
     if ($('.products__inner-box').length) {
         var mixer = mixitup('.products__inner-box');
